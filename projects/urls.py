fa30fb522f58549fb923d4d6589a4603ee2522ca
Otabekrasuljonov2006 <rasuljonovotabek45@gmail.com>
2025-12-25 16:21:11 +0500

    bunda men o'zgartirishni amalga oshirdim

--- projects/urls.py
+++ projects/urls.py
@@ -3,4 +3,7 @@ from .views import *
 urlpatterns = [
     path('projects/',projects, name = 'projects'),
     path('projects/<uuid:id>',project, name = 'project'),
+    path('project_add/',project_add, name = 'project_add'),
+    path('project_edit/<uuid:id>',project_edit, name = 'project_edit'),
+    path('project_delete/<uuid:id>',project_delete, name = 'project_delete'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from . models import *
+from . forms import ProjectForm
 # Create your views here.
 def projects(request):
     reviews_p = Review.objects.filter(value = '+')
@@ -20,4 +21,34 @@ def project(request, id):
         'tags' : tags
     }
     return render(request, "projects/project.html", context)
+def project_add(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    form = ProjectForm()
+    context = {
+        'form': form
+    }
+    return render(request, "projects/project_add.html", context)
+
+def project_edit(request, id):
+    project = Project.objects.get(id=id)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    context = {
+        'form': form
+    }
+    return render(request, "projects/project_add.html", context)
+def project_delete(request, id):
+    project = Project.objects.get(id=id)
+    project.delete()
+    return redirect('projects')
 
